9751d3dea8df9cf027095398fed8181da04cdba9
Vicidomini Lab <35b406e9.istitutoitalianotecnologia.onmicrosoft.com@emea.teams.ms>
2024-09-19 12:34:19 +0200

    Update CNNx1_transfer.py

--- CNN_code/CNNx1_transfer.py
+++ CNN_code/CNNx1_transfer.py
@@ -24,8 +24,7 @@ def transfer_cnnx1(path_weights,path_file,new_file,new_label,t_dset,t_labe):
     loaded_model.load_weights(path_weights)
     print("Loaded model from disk")
     
-    data = np.delete(np.load(new_file), 12, axis=3)[:,:,:,:,1]
-    labe = np.load(new_label)
+    data = np.delete(new_file, 12, axis=3)[:,:,:,:,1]
     
     print('TransferLearning')
     
@@ -35,7 +34,7 @@ def transfer_cnnx1(path_weights,path_file,new_file,new_label,t_dset,t_labe):
     loaded_model.compile(optimizer= tf.optimizers.Adam(learning_rate), loss='mse', metrics=[tf.keras.metrics.MeanSquaredError()])
     s_e = int(np.shape(data)[0]) - int((np.shape(data)[0])*20/100)
     with tf.device('/CPU:0'):
-        history = loaded_model.fit(data[0:s_e,:,:,:], labe[0:s_e,:], epochs=30, batch_size=16, shuffle = True, validation_data = (data[s_e::,:,:,:], labe[s_e::,:]))
+        history = loaded_model.fit(data[0:s_e,:,:,:], new_label[0:s_e,:], epochs=30, batch_size=16, shuffle = True, validation_data = (data[s_e::,:,:,:], new_label[s_e::,:]))
     
     print('Launch')
     
@@ -57,7 +56,6 @@ def transfer_cnnx1(path_weights,path_file,new_file,new_label,t_dset,t_labe):
    # loaded_model.save_weights(r'path\modelx1_transfer.weights.h5')
    # print("Saved model to disk")
     
-    
     #%% Load weights in TransferLearning folder
     
     json_file = open(r'files\modelx1_transfer.json', 'r')
@@ -70,8 +68,7 @@ def transfer_cnnx1(path_weights,path_file,new_file,new_label,t_dset,t_labe):
     
     #%% Testing
 
-    dset = np.delete(np.load(t_dset)[:,:,:,:,1], 12, axis=3)
-    labe = np.load(t_labe)
+    dset = np.delete(t_dset[:,:,:,:,1], 12, axis=3)
     
     with tf.device('/CPU:0'):
         pred_TL_x1 = loaded_model.predict(dset)
@@ -79,5 +76,5 @@ def transfer_cnnx1(path_weights,path_file,new_file,new_label,t_dset,t_labe):
     
     print('Testing')
     
-    return pred_TL_x1,labe
+    return pred_TL_x1,t_labe
    
